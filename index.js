cc3c85abc85aadf836055762b5df04515c81fde0
Bina Meir <binameir6@gmail.com>
2024-09-05 13:30:08 +0300

    frontend

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const PORT = process.env.PORT || 5000;
 // Serve static files from the 'client-build' directory
 app.use(express.static(path.join(__dirname +'/public')));
 
+// Catch-all route to serve React app for any other requests
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
